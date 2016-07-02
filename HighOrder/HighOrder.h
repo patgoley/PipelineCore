3566809128befdd6e8b269e4d212ce36bba266d3
Patrick Goley <patrick.goley@gmail.com>
2016-07-02 14:30:01 -0500

    Initial Commit

---
+++ HighOrder/HighOrder.h
@@ -0,0 +1,19 @@
+//
+//  HighOrder.h
+//  HighOrder
+//
+//  Created by Patrick Goley on 7/2/16.
+//  Copyright © 2016 patrickgoley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HighOrder.
+FOUNDATION_EXPORT double HighOrderVersionNumber;
+
+//! Project version string for HighOrder.
+FOUNDATION_EXPORT const unsigned char HighOrderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HighOrder/PublicHeader.h>
+
+
